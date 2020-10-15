6afff89d70b6a6479de83e2b31a1c39f278de10b
samiandreas70@gmail.com <samiandreas70@gmail.com>
2020-10-14 20:25:59 -0400

    created ajax call

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
     console.log("Hello");
 
+function betterDoc(){
+ var betterDocQueryURL : url 
 
+ $.ajax({
+    url: queryURL,
+    method: "GET"
+
+  }).then(function(response) {
 });
