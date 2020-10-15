aa9df9fd91942dc25cc3846b5a9eb7bab920081b
Elyse Wells <cewells89@gmail.com>
2020-10-14 20:21:49 -0400

    added ajax query for marta API

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
-    console.log("Hello");
+    // console.log("Hello");
 
 
+    // MARTA API
+    function getMartaAllBus(){
+
+        var martaBusQueryURL = "https://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus"
+
+        // AJAX Query for MARTA BUS API
+        $.ajax({
+            url: martaBusQueryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+
+            // Console logging response parameters
+        });
+
+    };
+
+    getMartaAllBus();
 });
