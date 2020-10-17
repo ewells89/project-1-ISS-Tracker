13e504b4a99808808ab18d6fd7d928ea35b94782
Hayes11 <bhayes81@gmail.com>
2020-10-17 17:24:23 -0400

    added variables for apiKey and query URL

--- script.js
+++ script.js
@@ -79,6 +79,17 @@ $(document).ready(function () {
         console.log(cityLocation);
         cityForecast(cityLocation);
 
+        //weather function 
+        function cityForcast(cityLocationValue) {
+            var apiKey = "63de61e390b4a0f5e75ff9df058d248b";
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+                cityLocationValue + "&appid=" + apiKey;
+            console.log(queryURL)
+
+            //ajax call
+
+
+        }
     });
 
     getISSLocation();
