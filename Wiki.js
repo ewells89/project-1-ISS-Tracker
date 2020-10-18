a5ee63c8cb1ae586a1f45871b559ffa98a6d01f7
edwinmunoz <edwin97munoz@gmail.com>
2020-10-18 14:36:53 -0400

    added javascript

--- Wiki.js
+++ Wiki.js
@@ -1,23 +1,12 @@
-$(document).ready(function () {
-    //console.log(hello)
-    //variables
-    var searchBar = document.getElementById('searchBar');
-    var searchBtn = document.getElementById('searchBtn');
-    var query = searchBar.value;
-    var api = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch"
+$(document).ready(function() {
+//console.log("hello")
+ var queryURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Craig%20Noone&format=json"
 
-    //functions
-    
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function(response) {
 
-  
-    $.ajax({
-       apikey: "api",
-       method: "GET",
-       datatype: "json"
-   }).then(function(response) {
-       console.log(response);
-   });
-
-   
+});
 
 });
