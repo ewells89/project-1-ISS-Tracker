2740b23397f6ef70964b246535f63fbf8b5e488d
Elyse Wells <cewells89@gmail.com>
2020-10-18 14:54:02 -0400

    modified jquery and ajax calls for wiki function

--- people-in-space.js
+++ people-in-space.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function () {
 
     // Function to get the list of people who are currently in space and which craft they are on.
@@ -6,23 +5,29 @@ $(document).ready(function () {
         $.getJSON('http://api.open-notify.org/astros.json', function (data) {
             console.log(data.number);
             console.log(data)
+
+
+            function getWiki(){
+                var queryURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Craig%20Noone&format=json"
+    
+                $.ajax({
+                    url: queryURL,
+                    method: "GET",
+                }).then(function(response) {
+                    console.log(response);
+                });
+
+            }
+
+            getWiki();
         });
 
-        
     }
 
     getPeople();
-=======
-$(document).ready(function() {
-//console.log("hello")
- var queryURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Craig%20Noone&format=json"
-
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response) {
 
 });
->>>>>>> 7780b7557165e5991589be42d4dc8730026e93a3
 
-});
+
+
+    
