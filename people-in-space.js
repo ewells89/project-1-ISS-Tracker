64612e784a5b25e02ac45b667a26976e46759584
edwinmunoz <edwin97munoz@gmail.com>
2020-10-19 19:57:01 -0400

    merging new changes

--- people-in-space.js
+++ people-in-space.js
@@ -5,11 +5,7 @@ $(document).ready(function () {
         $.getJSON('http://api.open-notify.org/astros.json', function (data) {
             console.log(data.number);
             console.log(data);
-
             
-
-            
-
             var peopleInSpace = data.people;
             console.log("Response Array");
             console.log(peopleInSpace);
@@ -19,20 +15,23 @@ $(document).ready(function () {
                 for(var i = 0; i < peopleInSpace.length; i++){
                     var person = data.people[i].name
                     console.log(person)
+
                 var queryURL = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=json"+"&srsearch="+person
                 //action=query&list=search&srsearch=Craig%20Noone&format=json"
-    
+               
                 $.ajax({
                     url: queryURL,
                     method: "GET",
                 }).then(function(response) {
                     console.log(response)
-                    
 
-
-                    
                     //console.log(data.people[i].name);
-                    $("astronaut-description").append($("<p=" + response.data[i].query.search.snippet +">"));
+
+                    $("search-btn").click(function () {
+
+                    $("astronaut-description").append($("<p> + response.data[i].query.search.snippet + </p>"));
+                   
+                });
                     
                 });
             }
